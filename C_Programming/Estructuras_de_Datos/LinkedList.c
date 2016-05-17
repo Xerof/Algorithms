0cb434fd7112805bcc74d6f348915ac086c34c66
Fernando Reyna <fernando.m.reyna.diaz@intel.com>
2016-05-17 17:05:49 -0500

    Agregado menu de listas
    Agregado el menu para listas y tambien se agrego codigo
    para manajer listas enlazadas circulares.
    
    Signed-off-by: Xerof <fercho27@gmail.com>

--- C_Programming/Estructuras_de_Datos/LinkedList.c
+++ C_Programming/Estructuras_de_Datos/LinkedList.c
@@ -1,47 +1,23 @@
 #include "LinkedList.h"
 
-void imprimirLinkedList(list *l)
+link regresaElemento(list *l, int indice)
 {
-	link tmpList;
-	int i = 0;
-	for (tmpList = l; tmpList != NULL; tmpList = tmpList->next)
-		printf("%d - %d \n",i++, tmpList->elemento);
-}
+	link n1;
+	link n2;
+	n1 = n2 = l;
 
-void imprimirListaCircular(list *l)
-{
-	link tmp = l->next;
-	while (tmp != l )
-	{
-		printf("- %d \n",tmp->elemento);
-		tmp = tmp->next;
-	}
-}
-int main (int argc, char **argv)
-{
-	int i;
-	list *myList = (list *) calloc(1,sizeof(list));
-	list *CircleList = (list *) calloc(1,sizeof(list));
-	CircleList->next = CircleList;
-	link x = CircleList;
-	insertar(&CircleList, 0);
-	//insertar(&myList, 0);
-	for (i = 1; i < 10; ++i) {
-		//insertar(&myList, i);
-		insertar(&CircleList,i);
+	for(;n2 != (void *) 0; n2 = n2->next) {
+		if(indice) {
+			indice--;
+			continue;
+		}
+		n1=n1->next;
 	}
 
-	imprimirListaCircular(CircleList);
-	link l = buscar(myList, 5);
-	if(l)
-		printf("Lo encontre %d\n ", l->elemento);
-
-	//imprimirLinkedList(myList);
-
-	return EXIT_SUCCESS;
+	return n1;
 }
-// C-> B -> A
-void insertar(list **l, int elemento)
+
+void insertarCircular(list **l, int elemento)
 {
 	link inicio  = (*l);
 	while(inicio->next != (*l))
@@ -52,6 +28,138 @@ void insertar(list **l, int elemento)
 	tmpList->elemento = elemento;
 	tmpList->next = *l;
 	inicio->next = tmpList;
+}
+
+void imprimirListaCircular(list *l)
+{
+	link tmp = l;
+	printf("Lista Circular:\nInicio:");
+	while (tmp->next != l )
+	{
+		printf("%d->",tmp->elemento);
+		tmp = tmp->next;
+	}
+	printf("%d->End\n",tmp->elemento);
+}
+
+link buscarCircularList(list *l, int elemento)
+{
+	link x = l;
+	while(x->next != l)
+	{
+		if(x->elemento == elemento)
+			return x;
+			x = x->next;
+	}
+
+	return NULL;
+}
+int printMenu()
+{
+	int opcion = 0;
+	do{
+		printf("======Listas Enlazadas=====\n");
+		printf("\tMenu:\n");
+		printf("\t1.- Lista Enlazada\n");
+		printf("\t2.- Lista Enlazada Circular\n");
+		printf("\t3.- Salir\n");
+		printf("\t\t Opcion: ");
+		scanf("%d", &opcion);
+	}while(opcion <= 0 || opcion > 3);
+
+	return opcion;
+}
+int printMenuLista(const char *titulo)
+{
+	int opcion;
+	do{
+		printf("====%s====\n",titulo);
+		printf("\t 1.- Insertar Manualmente\n");
+		printf("\t 2.- Insertar Aleatoriamente\n");
+		printf("\t 3.- Buscar\n");
+		printf("\t 4.- Imprimir\n");
+		printf("\t 5.- Revertir\n");
+		printf("\t\t Opcion: ");
+		scanf("%d",&opcion);
+	}while(opcion <= 0 || opcion > 6);
+
+	return opcion;
+}
+int main (int argc, char **argv)
+{
+	int i;
+	int opcion = printMenu();
+
+	do
+	{
+		switch(opcion)
+		{
+			case 1:
+			{
+				opcion = 0;
+				int opc = printMenuLista("Lista Ligada");
+				switch(opc)
+				{
+					case 1:
+					{
+						printf("Insercion Manual\n");
+					}break;
+					case 2:
+					{
+						printf("Insercion Aleatoria\n");
+					}break;
+					case 3:
+					{
+						printf("Buscar\n");
+					}break;
+					case 4:
+					{
+						printf("Imprimir\n");
+					}break;
+					case 5:
+					{
+						printf("Revertir\n");
+					}break;
+
+					default:
+						break;
+				}
+			}break;
+
+			case 2:
+			{
+				printf("Lista Enlazada Circular\n");
+			}break;
+
+			default:
+			{
+				opcion = printMenu();
+			}
+		}
+	}while(opcion != 3);
+
+	list *myList = (list *) calloc(1,sizeof(list));
+	list *CircleList = (list *) calloc(1,sizeof(list));
+	CircleList->next = CircleList;
+	//insertarCircular(&CircleList, 0);
+	//insertar(&myList, 0);
+	for (i = 1; i < 10; ++i) {
+		insertar(&myList, i);
+		insertarCircular(&CircleList,i);
+	}
+
+	imprimirListaCircular(CircleList);
+
+	imprimirLinkedList(myList);
+	return EXIT_SUCCESS;
+}
+
+void insertar(list **l, int elemento)
+{
+	link tmpList = (list *) malloc(sizeof(list));
+	tmpList->elemento = elemento;
+	tmpList->next = *l;
+	*l = tmpList;
 }
 
 link buscar(list *l, int elemento)
@@ -72,4 +180,15 @@ void borrar(list **l, int elemento)
 link predecesor(list *l, int elemento)
 {
 }
+
+void imprimirLinkedList(list *l)
+{
+	link tmpList;
+	int i = 0;
+	printf("Inicio:");
+	for (tmpList = l; tmpList != NULL; tmpList = tmpList->next)
+		printf("%d->",tmpList->elemento);
+	printf("Fin\n");
+}
+
 
--- C_Programming/Estructuras_de_Datos/LinkedList.h
+++ C_Programming/Estructuras_de_Datos/LinkedList.h
@@ -10,4 +10,8 @@ void insertar(list **l, int elemento);
 link buscar(list *l, int elemento);
 void borrar(list **l, int elemento);
 link predecesor(list *l, int elemento);
-
+void imprimirLinkedList(list *l);
+/* Lista Circular */
+void insertarCircular(list **l, int elemento);
+void imprimirListaCircular(list *l);
+link buscarCircularList(list *l, int elemento);
